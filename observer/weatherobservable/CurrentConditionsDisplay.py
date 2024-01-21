ada888045b4431165dc61c1cee2517416813a1cd
keeliputti <68512451+keeliputti@users.noreply.github.com>
2024-01-21 15:05:04 +0530

    Update CurrentConditionsDisplay.py

--- observer/weatherobservable/CurrentConditionsDisplay.py
+++ observer/weatherobservable/CurrentConditionsDisplay.py
@@ -10,13 +10,16 @@ class CurrentConditionsDisplay(Observer, DisplayElement):
     __humidity: float
         
     def __init__(self, observable: Observable):
+        # Our constructor now takes an Observable and we use this to add the current conditions object as an Observer.
         self.observable = observable
         self.observable.addObserver(self)
         
     def update(self, obs: Observable, arg: object):
+        # We’ve changed the update() method to take both an Observable and the optional data argument.
         if isinstance(obs, WeatherData):
             weatherData: WeatherData = obs
-            self.__temperature = weatherData.getTemperature()
+            # pull happening here. If 'arg' was used instead, it would have been push from Subject's side
+            self.__temperature = weatherData.getTemperature() 
             self.__humidity = weatherData.getHumidity()
             self.display()
             
