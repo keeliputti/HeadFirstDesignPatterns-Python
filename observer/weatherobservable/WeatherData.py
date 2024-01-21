4fcc10d3ba446fa0d8e9d7bb221b265930fc2aac
keeliputti <68512451+keeliputti@users.noreply.github.com>
2024-01-21 14:56:17 +0530

    Update WeatherData.py

--- observer/weatherobservable/WeatherData.py
+++ observer/weatherobservable/WeatherData.py
@@ -9,11 +9,14 @@ class WeatherData(Observable):
     def __init__(self):
         super().__init__()
 
-"""
-Notice we aren’t sending a data object with the notifyObservers() call. That means we’re using the PULL model.
-"""
+# We don’t need to keep track of our observers anymore, or manage their registration and removal, 
+# (the superclass will handle that) so we’ve removed the code for register, add and notify.
+        
+# Notice we aren’t sending a data object with the notifyObservers() call. That means we’re using the PULL model.
+    
     def measurementsChanged(self) -> None:
         self.setChanged()
+        # We now first call setChanged() to indicate the state has changed before calling notifyObservers().
         self.notifyObservers()
         
     def setMeasurements(self, temperature: float, humidity: float, pressure: float) -> None:
@@ -21,7 +24,10 @@ Notice we aren’t sending a data object with the notifyObservers() call. That m
         self.__humidity = humidity
         self.__pressure = pressure
         self.measurementsChanged()
-        
+
+# These methods aren’t new, but because we are going to use “pull” we thought we’d remind you they are here. 
+# The Observers will use them to get at the WeatherData object’s state.
+           
     def getTemperature(self) -> float:
         return self.__temperature
     
